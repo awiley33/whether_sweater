1f05419ec1528e5d7dcc7d1caeb1e983bc4b4d57
Anna Wiley <awiley33@gmail.com>
2023-09-26 12:33:45 -0500

    feat: Refactor user serializer using gem

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,18 +1,4 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :email, :api_key
-
-
-#   def as_json
-#     {
-#   "data": {
-#     "type": "users",
-#     "id": "1",
-#     "attributes": {
-#       "email": "whatever@example.com",
-#       "api_key": "t1h2i3s4_i5s6_l7e8g9i10t11"
-#     }
-#   }
-# }
-#   end
 end
