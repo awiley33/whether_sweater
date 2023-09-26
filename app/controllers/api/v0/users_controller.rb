f116dea13ded0ae7af9200385cb2db623ebb6ece
Anna Wiley <awiley33@gmail.com>
2023-09-26 17:33:21 -0500

    Add testing and functionality for user login

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,9 +1,10 @@
 class Api::V0::UsersController < ApplicationController
   def create
-    # user = User.create({email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: set_api_key})
     user = User.create!(user_params.merge(api_key: set_api_key))
     render json: UserSerializer.new(user), status: 201
   end
+
+
 
   private
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
 
+  include SessionsHelper
+
   def not_found_response(error)
     render json: ErrorSerializer.new(ErrorMessage.new(error.message, 404)).serialize_json, status: 404
   end
